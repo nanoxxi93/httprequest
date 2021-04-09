69ca779a7ae75fd463661d015292106d214e2547
nanoxxi93 <hernandoarima@gmail.com>
2021-04-09 09:27:06 -0500

    Init load

---
+++ Controllers/UserController.cs
@@ -0,0 +1,106 @@
+﻿using HttpRequest.Entities;
+using HttpRequest.Models;
+using HttpRequest.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpRequest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private IUserService _userService;
+        IConfiguration configuration;
+
+        public UserController(IUserService userService, IConfiguration configuration)
+        {
+            _userService = userService;
+            this.configuration = configuration;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody]User userParam)
+        {
+            var user = await _userService.Authenticate(userParam.Username, userParam.Password);
+
+            if (user == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(user);
+        }
+
+        [Authorize(AuthenticationSchemes = "BasicAuthentication")]
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            var users = await _userService.GetAll();
+            return Ok(users);
+        }
+
+        [AllowAnonymous]
+        [HttpPost("bearerpost")]
+        public async Task<IActionResult> Bearer([FromBody]User userParam)
+        {
+            var user = await _userService.Authenticate(userParam.Username, userParam.Password);
+
+            if (user == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return BuildToken(user);
+        }
+
+        private IActionResult BuildToken(User userParam)
+        {
+            try
+            {
+
+                Claim[] claims = new[] {
+                new Claim(JwtRegisteredClaimNames.UniqueName, userParam.FirstName + " " + userParam.LastName),
+                new Claim(JwtRegisteredClaimNames.NameId, userParam.Id.ToString())
+                };
+
+                ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, "Token");
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["SymmetricKey"]));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                var expiring = DateTime.UtcNow.AddMinutes(1);
+
+                JwtSecurityToken token = new JwtSecurityToken(
+                    claims: claimsIdentity.Claims,
+                    signingCredentials: creds
+                    );
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = expiring,
+                    result = userParam
+                });
+            }
+            catch (Exception ex)
+            {
+                return Json(ex);
+            }
+        }
+
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpGet("bearerget")]
+        public async Task<ActionResult> GetAllBearer()
+        {
+            var users = await _userService.GetAll();
+            return Ok(users);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,60 @@
+﻿using HttpRequest.Helpers;
+using HttpRequest.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Text;
+
+namespace HttpRequest
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddAuthentication("BasicAuthentication")
+                .AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>("BasicAuthentication", null);
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateAudience = false,
+                        ValidateIssuer = false,
+                        ValidateLifetime = false,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["SymmetricKey"]))
+                        //ClockSkew = TimeSpan.Zero
+                    }
+                );
+            // configure DI for application services
+            services.AddScoped<IUserService, UserService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
